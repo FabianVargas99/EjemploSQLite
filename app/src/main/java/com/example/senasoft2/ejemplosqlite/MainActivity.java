57b04b4afa7f3dca5ec7346a82a50a8e8e424048
SenaSoft 2 <vargasfabian243@gmail.com>
2018-09-05 07:32:58 -0500

    sdd

---
+++ app/src/main/java/com/example/senasoft2/ejemplosqlite/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.senasoft2.ejemplosqlite;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
